d47e1b193d9447ac9620aad599301e2ab72a0141
Evgeniy Kontcevoy <e.kontsevoy@foxford.ru>
2021-11-08 13:51:51 +0300

    update commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,78 +6,74 @@ import java.io.IOException;
 
 public class Main {
 
+    private static StringBuilder stringBuilder = new StringBuilder();
+
     public static void main(String[] args) {
 
-        StringBuilder stringBuilder = new StringBuilder();
+        File dirGames = new File("Games");
+        newDir(dirGames);
 
-        File dirSrt = new File("E://Games//srt");
-        if (dirSrt.mkdir())
-            stringBuilder.append("Каталог " + dirSrt.getName() + " создан" + "\n");
+        File dirSrt = new File("Games//srt");
+        newDir(dirSrt);
 
-            File dirStrMain = new File("E://Games//srt//main");
-            if (dirStrMain.mkdir())
-                stringBuilder.append("Каталог " + dirStrMain.getName() + " создан" + "\n");
+        File dirStrMain = new File("Games//srt//main");
+        newDir(dirStrMain);
 
-                File fileMain = new File("E://Games//srt//main//Main.java");
-                try {
-                 if (fileMain.createNewFile())
-                     stringBuilder.append("Файл " + fileMain.getName() + " создан" + "\n");
-                }catch (IOException ex){
-                 System.out.println(ex.getMessage());
-                }
+        File fileMain = new File("Games//srt//main//Main.java");
+        newFile(fileMain);
 
-                File fileUtils = new File("E://Games//srt//main//Utils.java");
-                 try {
-                      if (fileUtils.createNewFile())
-                          stringBuilder.append("Файл " + fileUtils.getName() + " создан" + "\n");
-                 }catch (IOException ex){
-                 System.out.println(ex.getMessage());
-                 }
+        File fileUtils = new File("Games//srt//main//Utils.java");
+        newFile(fileUtils);
 
-        File dirSrtTest = new File("E://Games//srt//test");
-        if (dirSrt.mkdir())
-            stringBuilder.append("Каталог " + dirSrtTest.getName() + " создан" + "\n");
+        File dirSrtTest = new File("Games//srt//test");
+        newDir(dirSrtTest);
 
+        File dirRes = new File("Games//res");
+        newDir(dirRes);
 
-        File dirRes = new File("E://Games//res");
-        if (dirRes.mkdir())
-            stringBuilder.append("Каталог " + dirRes.getName() + " создан" + "\n");
+        File dirResDrawables = new File("Games//res//drawables");
+       newDir(dirResDrawables);
 
-            File dirResDrawables = new File("E://Games//res//drawables");
-            if (dirResDrawables.mkdir())
-                stringBuilder.append("Каталог " + dirResDrawables.getName() + " создан" + "\n");
+        File dirResVectors = new File("Games//res//vectors");
+        newDir(dirResVectors);
 
-            File dirResVectors = new File("E://Games//res//vectors");
-            if (dirResVectors.mkdir())
-                stringBuilder.append("Каталог " + dirResVectors.getName() + " создан" + "\n");
+        File dirResIcons = new File("Games//res//icons");
+        newDir(dirResIcons);
 
-            File dirResIcons = new File("E://Games//res//icons");
-            if (dirResIcons.mkdir())
-                stringBuilder.append("Каталог " + dirResIcons.getName() + " создан" + "\n");
+        File dirSavegames = new File("Games//savegames");
+        newDir(dirSavegames);
 
+        File dirTemp = new File("Games//temp");
+        newDir(dirTemp);
 
-        File dirSavegames = new File("E://Games//savegames");
-        if (dirSavegames.mkdir())
-            stringBuilder.append("Каталог " + dirSavegames.getName() + " создан" + "\n");
+        File fileTemp = new File("Games//temp//temp.txt");
+        newFile(fileTemp);
 
-        File dirTemp = new File("E://Games//temp");
-        if (dirTemp.mkdir())
-            stringBuilder.append("Каталог " + dirTemp.getName() + " создан" + "\n");
-
-            File fileTemp = new File("E://Games//temp//temp.txt");
-            try {
-                if (fileTemp.createNewFile())
-                    stringBuilder.append("Файл " + fileTemp.getName() + " создан" + "\n");
-            }catch (IOException ex){
-                System.out.println(ex.getMessage());
-            }
-
-        try(FileWriter writer = new FileWriter("E://Games//temp//temp.txt", false)) {
+        try (FileWriter writer = new FileWriter("Games//temp//temp.txt", false)) {
             writer.write(stringBuilder.toString());
-        }
-        catch (IOException ex){
+            writer.flush();
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+
+    }
+
+    public static void newFile(File file){
+        try {
+            if (file.createNewFile()){
+                stringBuilder.append("Файл " + file.getName() + " создан" + "\n");
+            }else stringBuilder.append("невозможно создать файл " + file.getName() + "\n");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void newDir(File file){
+        if (file.mkdir()){
+            stringBuilder.append("Каталог " + file.getName() + " создан" + "\n");
+        }else stringBuilder.append("невозможно создать каталог " + file.getName() +  "\n");
+
+
 
     }
 }
